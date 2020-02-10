eb89bd90089cc24e57ef83065f3abef743a16e2d
smaldera <maldera@to.infn.it>
2020-02-10 10:55:12 +0000

    update

--- scanRecPars/analize/lancia_analisiSimo.py
+++ scanRecPars/analize/lancia_analisiSimo.py
@@ -1,11 +1,35 @@
 import subprocess
 
 
+base_dir='/data1/maldera/IXPE_work/rec_optimization/scanZeroThr_v2/'
+max_iter=19+1
 
-#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanZeroThr/'
-base_dir='/data1/maldera/IXPE_work/rec_optimization/scanMoma1Thr/'
+#scanMoma1Thr
+#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanMoma1Thr/'
+#max_iter=21
 
-dirs=['001361',  '001388',  '001416',  '001436',  '001461',  '001471']
+#scanMoma2Thr
+#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanMoma2Thr/'
+#max_iter=21                                                                                                                                      
+
+#Rmin
+#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanRmin/'
+#max_iter=18
+
+#Rmax
+#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanRmax/'
+#max_iter=22
+
+
+#exponential weight
+#base_dir='/data1/maldera/IXPE_work/rec_optimization/scanWs/'
+#max_iter=21
+
+
+
+
+
+dirs=['001333','001361',  '001388',  '001416',  '001436',  '001461',  '001471']
 #dirs=['001333']
 
 
@@ -17,7 +41,7 @@ for dirName in dirs:
     out_dir=base_dir+dirName+'/'
 
     
-    for  i in range (1,21):
+    for  i in range (1,max_iter):
         work_dir=out_dir+str(i)
         files_string=work_dir+'/*.lv1_recon.fits' 
         cmd='python plotAll_simo.py '+files_string+' -o '+work_dir+'/'
